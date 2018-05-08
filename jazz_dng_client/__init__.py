f6a7237dbcca39235d6b952840e0f44e7120c370
Paul Hanchett <pfhanchx@intel.com>
2018-05-08 16:20:10 -0700

    removed unneeded directory

--- jazz_dng_client/__init__.py
+++
@@ -1,7 +0,0 @@
-# import lxml as etree
-# import re
-# import utility_funcs.logger_yaml as log
-# from jazz.dng import Jazz
-from .dng import Jazz, JAZZ_CONFIG_PATH
-from .artifacts import Folder, Requirement, Collection, GenericRequirement
-
